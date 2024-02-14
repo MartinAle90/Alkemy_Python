0fffa50e978618ae454f6a96d9328187e59632cd
MartinAle90 <martinale90@gmail.com>
2024-02-14 17:23:01 -0300

    codigo para sumar y restar numero ingresados por el usuario

--- 01 - Fundamentos de Python/ejercicioClase3.py
+++ 01 - Fundamentos de Python/ejercicioClase3.py
@@ -6,27 +6,33 @@
 # Si el usuario ingresa el nro 1, realiza la acción A.
 # Si el usuario ingresa el nro 2, realiza la acción B.
 
-
-print ("1 - Accion A")
-print ("2 - Accion B")
-opcion = input('ingrese un numero: ')
-
-
 def calculadoraAritmetica(opcion):
 
-    if (1):
-        print("Accion A")
-        print("suma de 5 mas 1 hasta llegar a 10")
-        suma = 5
-        while (suma<10):
-            suma +=1
+    if (opcion==1):
+        print("1 - Suma de dos numeros ")
+        print("La Suma de dos numeros definidos por el usuario")
+        #print("ingrese el primer numero: ")
+        #num1 = input()
+        num1 = int(input("ingrese el primer numero: "))
+        #print("ingrese el segundo numero: ")
+        #num2 = input()
+        num2 = int(input("ingrese el segundo numero: "))
+        suma = num1 + num2
+        print("el resultado de la suma de", num1, "mas", num2, "es", suma)
+        suma = num1 + num2
 
-    elif (2):
-        print("Accion B")
-        print("resta de 5 menos 1 hasta llevar a cero")
-        resta = 6
-        while (resta>0):
-            resta -=1
+    elif (opcion==2):
+        print("Resta de dos numeros")
+        print("La Resta de dos numeros definidos por el usuario")
+        num1 = int(input("ingrese el primer numero: "))
+        num2 = int(input("ingrese el segundo numero: "))
+        resta = num1 + num2
+        print("el resultado de la resta de", num1, "menos", num2, "es", resta)
 
     else:
         print("opcion no definida")
+
+print ("1 - Suma de dos numeros")
+print ("2 - Resta de dos numeros")
+opcion = int(input('ingrese una opcion: '))
+calculadoraAritmetica(opcion)
