37a97f6658c192a52bb1a0914dd03663b3f56971
MartinAle90 <martinale90@gmail.com>
2024-02-14 16:47:53 -0300

    codigo base

---
+++ 01 - Fundamentos de Python/ejercicioClase3.py
@@ -0,0 +1,21 @@
+# Consigna: ✍️
+# Crear un pequeño programa que realice una función aritmética que permita al usuario ingresar datos por la terminal acorde a distintas opciones.
+# Para ellos debemos definir una función que reciba parámetros:
+# Combinar funciones nativas y funciones definidas,
+# condicionales, y bucles.
+# Si el usuario ingresa el nro 1, realiza la acción A.
+# Si el usuario ingresa el nro 2, realiza la acción B.
+
+
+print ("1 - Accion A")
+print ("2 - Accion B")
+opcion = input('ingrese un numero: ')
+
+
+def calculadoraAritmetica(opcion):
+    if (1):
+        print("Accion A")
+    elif (2):
+        print("Accion B")
+    else:
+        print("opcion no definida")
